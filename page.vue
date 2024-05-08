ad522d8e3539761010ef48e9cabfca75fdcbfd0a
Denis Butsky <30866948+nthbutsky@users.noreply.github.com>
2024-05-08 04:02:39 +0300

    Create page.vue

---
+++ page.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col">
+    <div class="font-5 text-color-2">
+      {{ t("common.title") }}
+    </div>
+
+    <span class="font-2-header text-color-1">
+      {{ t("common.subtitle") }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {
+  translate,
+} from '@/lang/main';
+
+import {
+  usePageContext,
+} from '@/renderer/usePageContext';
+
+const pageContext = usePageContext();
+const l = pageContext.browserLanguages;
+
+const t = (path: string) => {
+  return translate(path, l);
+};
+</script>
