1b83750788bd88e7b74e4427307d89aad71e74d7
Denis Butsky <30866948+nthbutsky@users.noreply.github.com>
2024-08-30 10:31:57 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,15 @@
 import {
-  fr,
-} from './fr';
-import {
   en,
 } from './en';
+import {
+  fr,
+} from './fr';
 
-export enum ELocale {
-  FRENCH = 'fr-FR',
-  ENGLISH = 'en-EN',
-}
+export const LOCALE = {
+  ENGLISH: en.locale.short,
+  FRENCH: fr.locale.short,
+} as const;
+type TObjectValues<T> = T[keyof T];
+export type TLocale = TObjectValues<typeof LOCALE>;
 
-export const localeDefault = ELocale.FRENCH;
-
-export const defaultLanguage = (langList: readonly string[]) => {
-  return langList[0] === localeDefault;
-};
-
-export const translate = (path: string, langList: readonly string[]): string => {
-  const browserLanguage = langList[0] === localeDefault
-    ? ELocale.FRENCH
-    : ELocale.ENGLISH;
-  const parsePath = () => path.split('.');
-  const getProp = (object: any, propsArray: string[]) => {
-    return propsArray.reduce((acc, key) => (acc && acc[key]), object) || path;
-  };
-
-  switch (browserLanguage) {
-    case ELocale.FRENCH:
-      return getProp(fr, parsePath());
-    case ELocale.ENGLISH:
-      return getProp(en, parsePath());
-    default:
-      return getProp(fr, parsePath());
-  }
-};
+export const localeDefault = LOCALE.ENGLISH;
